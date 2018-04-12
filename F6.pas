e942fdfeae93e66de29f7544d9f5ac085e43cbcd
pujivalenc <pujivalenc@users.noreply.github.com>
2018-04-12 18:28:36 +0700

    IESUTOAw

--- F6.pas
+++ F6.pas
@@ -1,24 +1,60 @@
 unit F6;
 interface
 	uses uDef,F1;//untuk ngambil data
-	procedure olahbahan(a:Abahanolahan;var b:Ainventori;var c : Asimulasi);
+	procedure olahbahan(olah:Abahanolahan;var olahb,mentah:Ainventori;var c : Asimulasi);
 implementation
-	procedure olahbahan(a:Abahanolahan;var b:Ainventori;var c : Asimulasi);
+	procedure olahbahan(olah:Abahanolahan;var olahb,mentah:Ainventori;var c : Asimulasi);
 	var
 	nama:string;
-	jlh,i,j:longint;
-	cukup:=boolean;
+	i,j,count,k:longint;
+	ketemu:boolean;
 	begin
 			writeln('bahan yang mau dibuat : ');
 			readln(nama);
-			writeln('kuantitas : ');
-			readln(jlh);
 			i:=1;
-			while (a[i].nama <> nama) do
+			while (olah[i].nama <> nama) do//mencari dimana bahan olahan yang ingin dibuat pada kamus
 			begin
 					i:=i+1;
 			end;
-			cukup:=True;
-			for j:=1 to a[i].n do 
+			count:=0;//untuk bahan yang mencukupi
+			for j:=1 to olah[i].n do 
 			begin
+				ketemu:=False;//var untuk nyari
+				k:=1;
+				while((ketemu=False)and(k< NInvBM)) do 
+				begin
+					if((olah[i].bahan[j]=mentah[k].nama) and (mentah[k].jumlah>0)) then
+					begin
+						ketemu:=True;
+						count:=count+1;
+					end;
+					k:=k+1;
+				end;
+			end;
+			if((count=olah[i].n)and(c[NSim].tEnergi>0)) then
+			begin
+				for j:=1 to olah[i].n do 
+				begin
+					ketemu:=False;//var untuk nyari
+					k:=1;
+					while((ketemu=False)and(k< NInvBM)) do 
+					begin
+						if((olah[i].bahan[j]=mentah[k].nama) and (mentah[k].jumlah>0)) then
+						begin
+							ketemu:=True;
+							mentah[k].jumlah:=mentah[k].jumlah-1;//ngurangi jumlah bahan mentah di inventori
+						end;
+						k:=k+1;
+					end;
+				end;
+				c[NSim].tEnergi:=c[NSim].tEnergi-1;
+				c[NSim].tBOlahanDibuat:=c[NSim].tBOlahanDibuat+1;
+				//msih perlu tanggal
+			end else writeln('gak bisa beli');
+	end;
+end.
+				
+				
+				
+				
 				
