7a6aadf34912a79e32342010a774e2b0381ec33b
boasmarbun <38323100+boasmarbun@users.noreply.github.com>
2018-04-14 12:30:35 +0700

    Add files via upload

---
+++ delete_array.pas
@@ -0,0 +1,29 @@
+unit delete_array;
+
+interface
+
+uses uDef,F1;
+
+	procedure deletearray (var arr : Ainventori; var Neff : Integer; var index : Integer);
+	
+implementation
+
+procedure deletearray (var arr : Ainventori; var Neff : Integer; var index : Integer);
+	begin	
+		if (Neff=index) then
+			begin
+				Neff:=Neff-1; {delete elemen terakhir}
+			end else
+			begin
+				for i := index to Neff-1 do
+					begin
+						arr[i] := arr[i+1];
+					end;
+			end;
+		Neff:=Neff-1; { Mengeset panjang array menjadi panjang array-1 }
+	end;
+end.
+//Immplementasi
+	//Inventori bahan mentah
+	
+	//deletearray ( FEInventoribahanmentah[NomorSim],NInvBM[NomorSim], i):
