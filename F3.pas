7ae8b106bb68707c58a536279c40227cd53b6866
Kevin <kevinnathaniel497@yahoo.com>
2018-04-22 22:09:18 +0700

    Latest

--- F3.pas
+++ F3.pas
@@ -8,7 +8,7 @@ interface
 uses uDef, sysutils, Dateutils, restock, delete_array, C_Maks;
 
 	{ Deklarasi Fungsi/Prosedur }
-	
+
 	procedure startSimulasi(var countis, countm, aksi: Integer; FESimulasi: ASimulasi; NomorSim: Integer; var FEBahanMentah: Abahanmentah; NBahanM: Integer; var FEInventoriBahanMentah, FEInventoriBahanOlahan: AInventori; var NInvBM, NInvBO: integer);
 	{Prosedur yang menyiapkan array inventori bahan mentah, array inventori bahan olahan, dan array bahan mentah}
 	{I.S.: Nomor Simulasi yang ingin dijalankan terinisialisasi}
@@ -67,12 +67,12 @@ implementation
 		begin
 			if (NInvBO>=1) then
 			begin
-				j:=NInvBO
+				j:=NInvBO;
 				deletearray(FEInventoriBahanOlahan,NInvBO,j);
 			end else
 			if (NInvBM>=1) then
 			begin
-				j:=NInvBM
+				j:=NInvBM;
 				deletearray(FEInventoriBahanMentah,NInvBM,j);
 			end;
 		end;
