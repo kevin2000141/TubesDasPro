9edf47fb75cc7163454876f84af3cf75063d2fd7
Kevin <kevinnathaniel497@yahoo.com>
2018-04-22 12:33:53 +0700

    Update 22/04/2018, 12.25

--- F1.pas
+++ F1.pas
@@ -8,11 +8,14 @@ uses uDef, sysutils;
 	
 	var
 		a,b,c,d,e,f: Text;
-		i,j : Integer;
+		counterror, i,j : Integer;
 		S:String;
+		error: Boolean;
 	
 	{ Deklarasi Fungsi/Prosedur }
 	function shorten(panjang:AnsiString):AnsiString;
+
+	function jumlahchar(S: Ansistring): integer;
 
 	procedure load( var l : Boolean;
 					var FEBahanMentah: Abahanmentah; 
@@ -32,10 +35,30 @@ uses uDef, sysutils;
 implementation
 	
 	function shorten(panjang:AnsiString):AnsiString;
+	var
+		temp:Ansistring;
 	begin
-		shorten := StringReplace(panjang, ' | ', '|', [rfReplaceAll]);
+		temp := panjang;	
+		temp := StringReplace(panjang, ' | ', '|', [rfReplaceAll]);
+		temp := StringReplace(panjang, '| ', '|', [rfReplaceAll]);
+		temp := StringReplace(panjang, ' |', '|', [rfReplaceAll]);
+		shorten := temp;
 	end;
 
+	function jumlahchar(S: Ansistring): integer;
+	var
+		i: Integer;
+	begin
+		jumlahchar := 0;
+		for i := 1 to Length(S) do
+		begin
+			if S[i] = '|' then
+			begin
+				jumlahchar:=jumlahchar+1;
+			end;
+		end;
+		end;
+		
 	procedure load( var l : Boolean;
 					var FEBahanMentah: Abahanmentah; 
 					var FEBahanOlahan: Abahanolahan;
@@ -70,166 +93,377 @@ implementation
 		reset (f);
 		
 		NBahanM:=0;
+		counterror:=0;
 		while not(eof(a)) do
 			begin
+			error:=False;
 			NBahanM:=NBahanM+1;
 			readln (a, temp);
 			temp := shorten(temp);
+			if jumlahchar(temp) <> 3 then
+			begin
+				error:=True;
+			end;
 			FEBahanMentah[NBahanM].nama:=copy(temp, 1, (pos('|',temp) -1));
 			delete(temp,1,(pos('|',temp)));
-			FEBahanMentah[NBahanM].harga:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FEBahanMentah[NBahanM].harga) then
+			begin
+				FEBahanMentah[NBahanM].harga:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
 			delete(temp,1,(pos('|',temp)));
-			FEBahanMentah[NBahanM].kadaluarsa:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FEBahanMentah[NBahanM].kadaluarsa) then
+			begin
+				FEBahanMentah[NBahanM].kadaluarsa:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
 			delete(temp,1,(pos('|',temp)));
-			FEBahanMentah[NBahanM].jumlah:=StrToInt(temp);
+			if TryStrToInt(temp,FEBahanMentah[NBahanM].jumlah) then
+			begin
+				FEBahanMentah[NBahanM].jumlah:=StrToInt(temp);
+			end else
+			begin
+				error:=True;
+			end;
+			if error then
+			begin
+				NBahanM:=NBahanM-1;
+				counterror:=counterror+1;
+			end;
 			end;	
-		{for i:=1 to NBahanM do
+		if counterror > 0 then
 		begin
-			writeln(FEBahanMentah[i].nama, ' ', FEBahanMentah[i].harga, ' ', FEBahanMentah[i].kadaluarsa);
+			writeln(counterror, ' line pada file BahanMentah.txt tidak diload karena ada kesalahan data.');
 		end;
-		writeln();}
 
 		NBahanO:=0;
+		counterror:=0;
 		while not(eof(b)) do
 			begin
+			error:=False;
 			NBahanO:=NBahanO+1;
 			readln (b, temp);
 			temp := shorten(temp);
 			FEBahanOlahan[NBahanO].nama:=copy(temp, 1, (pos('|',temp) -1));
 			delete(temp,1,(pos('|',temp)));
-			FEBahanOlahan[NBahanO].harga:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FEBahanOlahan[NBahanO].n:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			if FEBahanOlahan[NBahanO].n>1 then 
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FEBahanOlahan[NBahanO].harga) then
 			begin
-				for i:=1 to (FEBahanOlahan[NBahanO].n - 1) do
+				FEBahanOlahan[NBahanO].harga:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FEBahanOlahan[NBahanO].n) then
+			begin
+				FEBahanOlahan[NBahanO].n:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+				delete(temp,1,(pos('|',temp)));
+				if jumlahchar(temp) <> (FEBahanOlahan[NBahanO].n-1) then
 				begin
-					FEBahanOlahan[NBahanO].bahan[i]:=(copy(temp, 1, (pos('|',temp) -1)));
-					delete(temp,1,(pos('|',temp)));
+					error:=True;
 				end;
-			end;
-			FEBahanOlahan[NBahanO].bahan[FEBahanOlahan[NBahanO].n]:=temp;
-			
-			end;
-		{for i:=1 to NBahanO do
-		begin
-			write(FEBahanOlahan[i].nama, ' ', FEBahanOlahan[i].harga, ' ', FEBahanOlahan[i].n);
-			for j:=1 to FEBahanOlahan[NBahanO].n do
+				if FEBahanOlahan[NBahanO].n>1 then 
+				begin
+					j:=FEBahanOlahan[NBahanO].n - 1;
+					for i:=1 to j do
+					begin
+						if StringReplace((copy(temp, 1, (pos('|',temp) -1))), ' ', '', [rfReplaceAll, rfIgnoreCase]) = '' then
+						begin
+							delete(temp,1,(pos('|',temp)));
+							FEBahanOlahan[NBahanO].n:=FEBahanOlahan[NBahanO].n-1;
+						end else
+						begin
+							FEBahanOlahan[NBahanO].bahan[i]:=(copy(temp, 1, (pos('|',temp) -1)));
+							delete(temp,1,(pos('|',temp)));
+						end;
+					end;
+					if StringReplace(temp, ' ', '', [rfReplaceAll, rfIgnoreCase]) <> '' then
+					begin
+						FEBahanOlahan[NBahanO].bahan[FEBahanOlahan[NBahanO].n]:=temp;
+					end;
+				end;
+			end else
 			begin
-				write(' ', FEBahanOlahan[i].bahan[j]);
+				error:=True;
 			end;
-			writeln();
+			if error then
+			begin
+				NBahanO:=NBahanO-1;
+				counterror:=counterror+1;
+			end;
+			end;
+		if counterror > 0 then
+		begin
+			writeln(counterror, ' line pada file BahanOlahan.txt tidak diload karena ada kesalahan data.');
 		end;
-		writeln();}
 
 		NSim:=0;
+		counterror:=0;
 		while not(eof(f)) do
 			begin
+			error:=False;
 			NSim:=NSim + 1;
 			readln (f, temp);
 			temp := shorten(temp);
-			FESimulasi[NSim].nomor:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			if jumlahchar(temp) <> 11 then
+			begin
+				error:=True;
+			end;
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].nomor) then
+			begin
+				FESimulasi[NSim].nomor:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
 			delete(temp,1,(pos('|',temp)));
 			DefaultFormatSettings.ShortDateFormat := 'd/m/yyyy';
 			DefaultFormatSettings.DateSeparator := '/';
-			FESimulasi[NSim].tanggal:=StrToDate(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].tHariHidup:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].tEnergi:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].maxInventori:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].tBMentahDibeli:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].tBOlahanDibuat:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].tBOlahanDijual:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].tResepDijual:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].tPemasukan:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].tPengeluaran:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FESimulasi[NSim].tUang:=StrToInt(temp);
+			if TryStrToDate((copy(temp, 1, (pos('|',temp) -1))),FESimulasi[NSim].tanggal) then
+			begin
+				FESimulasi[NSim].tanggal:=StrToDate(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:= True;
 			end;
-		{for i:=1 to NSim do
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].tHariHidup) then
+			begin
+				FESimulasi[NSim].tHariHidup:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].tEnergi) then
+			begin
+				FESimulasi[NSim].tEnergi:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].maxInventori) then
+			begin
+				FESimulasi[NSim].maxInventori:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].tBMentahDibeli) then
+			begin
+				FESimulasi[NSim].tBMentahDibeli:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].tBOlahanDibuat) then
+			begin
+				FESimulasi[NSim].tBOlahanDibuat:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].tBOlahanDijual) then
+			begin
+				FESimulasi[NSim].tBOlahanDijual:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].tResepDijual) then
+			begin
+				FESimulasi[NSim].tResepDijual:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].tPemasukan) then
+			begin
+				FESimulasi[NSim].tPemasukan:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FESimulasi[NSim].tPengeluaran) then
+			begin
+				FESimulasi[NSim].tPengeluaran:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(temp,FESimulasi[NSim].tUang) then
+			begin
+				FESimulasi[NSim].tUang:=StrToInt(temp);
+			end else
+			begin
+				error:=True;
+			end;
+			if error then
+			begin
+				NSim:=NSim-1;
+				counterror:=counterror+1;
+			end;
+			end;
+		if counterror > 0 then
 		begin
-			writeln(FESimulasi[i].nomor, ' ', FormatDateTime('ddddd',FESimulasi[NSim].tanggal), ' ', FESimulasi[NSim].tHariHidup, ' ', FESimulasi[NSim].tEnergi, ' ', FESimulasi[NSim].maxInventori, ' ', FESimulasi[NSim].tBMentahDibeli, ' ', FESimulasi[NSim].tBOlahanDibuat, ' ', FESimulasi[NSim].tBOlahanDijual, ' ',FESimulasi[NSim].tResepDijual, ' ', FESimulasi[NSim].tPemasukan, ' ', FESimulasi[NSim].tPengeluaran, ' ', FESimulasi[NSim].tUang);
-		end;}
-		
+			writeln(counterror, ' line pada file Simulasi.txt tidak diload karena ada kesalahan data.');
+		end;
+
 		NInvBM:=0;
+		counterror:=0;
 		while not(eof(c)) do
 			begin
+			error:=False;
 			NInvBM:=NInvBM + 1;
 			readln (c, temp);
 			temp := shorten(temp);
+			if jumlahchar(temp) <> 2 then
+			begin
+				error:=True;
+			end;
 			FEInventoriBahanMentah[NInvBM].nama:=copy(temp, 1, (pos('|',temp) -1));
 			delete(temp,1,(pos('|',temp)));
 			DefaultFormatSettings.ShortDateFormat := 'd/m/yyyy';
 			DefaultFormatSettings.DateSeparator := '/';
-			FEInventoriBahanMentah[NInvBM].tanggal:=StrToDate(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FEInventoriBahanMentah[NInvBM].jumlah:=StrToInt(temp);
+			if TryStrToDate((copy(temp, 1, (pos('|',temp) -1))),FEInventoriBahanMentah[NInvBM].tanggal) then
+			begin
+				FEInventoriBahanMentah[NInvBM].tanggal:=StrToDate(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
 			end;
-		
-		{for j:=1 to NInvBM do
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(temp,FEInventoriBahanMentah[NInvBM].jumlah) then
+			begin
+				FEInventoriBahanMentah[NInvBM].jumlah:=StrToInt(temp);
+			end else
+			begin
+				error:=True;
+			end;
+			if error then
+			begin
+				NInvBM:=NInvBM-1;
+				counterror:=counterror+1;
+			end;
+			end;
+		if counterror > 0 then
 		begin
-			writeln (FEInventoriBahanMentah[i,j].nama, ' ', FormatDateTime('ddddd',FEInventoriBahanMentah[i,j].tanggal), ' ', FEInventoriBahanMentah[i,j].jumlah);
+			writeln(counterror, ' line pada file InvBahanMentah.txt tidak diload karena ada kesalahan data.');
 		end;
-		writeln();}
 		
 		NInvBO:=0;
+		counterror:=0;
 		while not(eof(d)) do
 			begin
+			error:=False;
 			NInvBO:=NInvBO + 1;
 			readln (d, temp);
 			temp := shorten(temp);
+			if jumlahchar(temp) <> 2 then
+			begin
+				error:=True;
+			end;
 			FEInventoriBahanOlahan[NInvBO].nama:=copy(temp, 1, (pos('|',temp) -1));
 			delete(temp,1,(pos('|',temp)));
 			DefaultFormatSettings.ShortDateFormat := 'd/m/yyyy';
 			DefaultFormatSettings.DateSeparator := '/';
-			FEInventoriBahanOlahan[NInvBO].tanggal:=StrToDate(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FEInventoriBahanOlahan[NInvBO].jumlah:=StrToInt(temp);
+			if TryStrToDate((copy(temp, 1, (pos('|',temp) -1))),FEInventoriBahanOlahan[NInvBO].tanggal) then
+			begin
+				FEInventoriBahanOlahan[NInvBO].tanggal:=StrToDate(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
 			end;
-		{for j:=1 to NInvBO do
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(temp,FEInventoriBahanOlahan[NInvBO].jumlah) then
+			begin
+				FEInventoriBahanOlahan[NInvBO].jumlah:=StrToInt(temp);
+			end else
+			begin
+				error:=True;
+			end;
+			if error then
+			begin
+				NInvBO:=NInvBO-1;
+				counterror:=counterror+1;
+			end;
+			end;
+		if counterror > 0 then
 		begin
-			writeln (FEInventoriBahanOlahan[i,j].nama, ' ', FormatDateTime('ddddd',FEInventoriBahanOlahan[i,j].tanggal), ' ', FEInventoriBahanOlahan[i,j].jumlah);
+			writeln(counterror, ' line pada file InvBahanOlahan.txt tidak diload karena ada kesalahan data.');
 		end;
-		writeln();}
+
 
 		NResep:=0;
+		counterror:=0;
 		while not(eof(e)) do
 			begin
+			error:=False;
 			NResep:=NResep+1;
 			readln (e, temp);
 			temp := shorten(temp);
 			FEResep[NResep].nama:=copy(temp, 1, (pos('|',temp) -1));
 			delete(temp,1,(pos('|',temp)));
-			FEResep[NResep].harga:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			FEResep[NResep].n:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
-			delete(temp,1,(pos('|',temp)));
-			for i:=1 to (FEResep[NResep].n - 1) do
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FEResep[NResep].harga) then
 			begin
-				FEResep[NResep].bahan[i]:=(copy(temp, 1, (pos('|',temp) -1)));
+				FEResep[NResep].harga:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
+			end else
+			begin
+				error:=True;
+			end;
+			delete(temp,1,(pos('|',temp)));
+			if TryStrToInt(copy(temp, 1, (pos('|',temp) -1)),FEResep[NResep].n) then
+			begin
+				FEResep[NResep].n:=StrToInt(copy(temp, 1, (pos('|',temp) -1)));
 				delete(temp,1,(pos('|',temp)));
-			end;
-			FEResep[NResep].bahan[FEResep[NResep].n]:=temp;
-			end;
-		{for i:=1 to NResep do
-		begin
-			write(FEResep[i].nama, ' ', FEResep[i].harga, ' ', FEResep[i].n);
-			for j:=1 to FEResep[NResep].n do
+				if jumlahchar(temp) <> (FEResep[NResep].n-1) then
+				begin
+					error:=True;
+				end;
+				j:=FEResep[NResep].n - 1;
+				for i:=1 to j do
+				begin
+					if StringReplace((copy(temp, 1, (pos('|',temp) -1))), ' ', '', [rfReplaceAll, rfIgnoreCase]) = '' then
+					begin
+						delete(temp,1,(pos('|',temp)));
+						FEResep[NResep].n:=FEResep[NResep].n - 1;
+					end else
+					begin
+						FEResep[NResep].bahan[i]:=(copy(temp, 1, (pos('|',temp) -1)));
+						delete(temp,1,(pos('|',temp)));
+					end;
+				end;
+				if StringReplace(temp, ' ', '', [rfReplaceAll, rfIgnoreCase]) <> '' then
+				begin
+					FEResep[NResep].bahan[FEResep[NResep].n]:=temp;
+				end;
+			end else
 			begin
-				write(' ', FEResep[i].bahan[j]);
+				error:=True;
 			end;
-			writeln();
+			if error then
+			begin
+				NResep:=NResep-1;
+				counterror:=counterror+1;
+			end;
+			end;
+		if counterror > 0 then
+		begin
+			writeln(counterror, ' line pada file Resep.txt tidak diload karena ada kesalahan data.');
 		end;
-		writeln();}
+
 		close(a);
 		close(b);
 		close(c);
@@ -239,8 +473,8 @@ implementation
 		writeln('Loading file sukses.');
 		l:= True;	
 	end else
-		begin
-			writeln('Salah satu file tidak ada, loading file gagal.');
-		end;
+	begin
+		writeln('Salah satu file tidak ada, loading file gagal.');
+	end;
 	end;
 end.
