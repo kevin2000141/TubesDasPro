e776b86f73cecddd22adfc2e977c081df321e455
Kevin <kevinnathaniel497@yahoo.com>
2018-04-22 20:02:39 +0700

    Latest

--- restock.pas
+++ restock.pas
@@ -8,22 +8,32 @@ interface
 uses F1, sysutils, uDef;
 
 	procedure restockbahan(FESimulasi: ASimulasi; NomorSim: Integer; var FEBahanMentah: Abahanmentah; NBahanM: Integer);
-	
+	{Prosedur yang menambah jumlah stock bahan menjadi 25 pada file bahan mentah jika tanggal genap} 
+	{I.S. FESimulasi, NomorSim, FEBahanMentah, NBahanM terinisialiasai}
+	{F.S. Jumlah bahan di FEBahanMentah menjadi 25}
+
 implementation
 
 	procedure restockbahan(FESimulasi: ASimulasi; NomorSim: Integer; var FEBahanMentah: Abahanmentah; NBahanM: Integer);
+	{Prosedur yang menambah jumlah stock bahan menjadi 25 pada file bahan mentah jika tanggal genap}
+	{I.S. FESimulasi, NomorSim, FEBahanMentah, NBahanM terinisialiasai}
+	{F.S. Jumlah bahan di FEBahanMentah menjadi 25}
+
+	{KAMUS}
 	var
 		Date: TDateTime;
 		i: Integer;
 		year, month, day: Word;
+	
+	{ALGORITMA}
 	begin
 		Date:=FESimulasi[NomorSim].tanggal;
 		DecodeDate(Date,year,month,day);
-		if day mod 2 = 0 then
+		if day mod 2 = 0 then {Pengecekan apakah tanggal genap}
 		begin
 			for i:=1 to NBahanM do
 			begin
-				FEBahanMentah[i].jumlah:=25;
+				FEBahanMentah[i].jumlah:=25; {Untuk semua bahan stock menjadi 25}
 			end;
 		end;
 	end;
